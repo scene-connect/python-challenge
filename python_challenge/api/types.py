47aec4e45a4bec127838e2726bb966f7fe59c584
Daniel Winterstein <daniel@good-loop.com>
2024-10-24 13:06:01 +0100

    Challenge work: adds an /energy endpoint. This does not include a unit test.

--- python_challenge/api/types.py
+++ python_challenge/api/types.py
@@ -1,7 +1,12 @@
 import pydantic
 
+from python_challenge.types.retrofit_planner import RetrofitPlannerResponsePublic
+
 from ..types.home import Home
 
 
 class HomeDetailsResponse(pydantic.BaseModel):
     home: Home
+
+class EnergyRunResultsResponse(pydantic.BaseModel):
+    energy_run_results: RetrofitPlannerResponsePublic
