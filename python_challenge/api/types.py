b4c0547a772ce80e6475840a802ded24aff41ba2
Shreyas Bhargava <shreyas.bhargava@newvision-software.com>
2025-02-07 18:59:35 +0530

    python challenge

--- python_challenge/api/types.py
+++ python_challenge/api/types.py
@@ -1,7 +1,13 @@
 import pydantic
 
 from ..types.home import Home
+from ..types.retrofit_planner import RetrofitPlannerResponsePublic
 
 
 class HomeDetailsResponse(pydantic.BaseModel):
     home: Home
+
+
+class RetrofitPlannerResponse(pydantic.BaseModel):
+    results: RetrofitPlannerResponsePublic
+
--- python_challenge/api/urls.py
+++ python_challenge/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from . import PythonChallenge
 
 urlpatterns = [
     # Legacy URLs, to be deprecated in v5 ?
@@ -8,5 +9,10 @@ urlpatterns = [
         r"home/<str:uprn>",
         views.HomeDetailsByUPRN.as_view(),
         name="get-home",
+    ),
+    path(
+        r"python-chaalenge/<str:uuid>",
+        PythonChallenge.PythonChallenge.as_view(),
+        name="get-new",
     ),
 ]
