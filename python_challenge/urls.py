a15bb16183d75ca1d83f63aba797a799ea8c9e01
Howard Cox <dev.anubis@gmail.com>
2025-05-08 13:05:13 +0100

    Add root direct to API docs for simplicity

--- python_challenge/urls.py
+++ python_challenge/urls.py
@@ -1,5 +1,6 @@
 """Root URL Configuration."""
 
+from django.shortcuts import redirect
 from django.urls import include
 from django.urls import path
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
     path("docs/", include("python_challenge.docs.urls")),
     path("api/", include("python_challenge.api.urls")),
+    path("", lambda x: redirect("/docs/")),
 ]
